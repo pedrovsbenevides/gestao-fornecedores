de961db03809b23a685328ce1f9dfe62e488fe68
Pedro-Benevides <pvsbenevides197@gmail.com>
2025-04-28 22:31:11 -0300

    feat(repositories): implementa repos com métodos de create

---
+++ backend/repositories/DemandaRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace backend\repositories;
+
+require_once dirname(dirname(__FILE__)) . '\db\config\connection.php';
+require_once dirname(dirname(__FILE__)) . '\models\Demanda.php';
+
+use backend\db\config\Connection;
+use backend\models\Demanda;
+use PDO;
+
+class DemandaRepository
+{
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Connection::Connect();
+    }
+
+    public function create(array $data)
+    {
+        try {
+
+            $this->pdo->beginTransaction();
+
+            $createStatement = $this->pdo->prepare('INSERT INTO demandas (titulo, cep) VALUES (:titulo, :cep)');
+            $createStatement->bindParam(':titulo', $data['titulo']);
+            $createStatement->bindParam(':cep', $data['cep']);
+
+            $createStatement->execute();
+
+            $this->pdo->commit();
+
+            return $this->buildDemanda($this->pdo->lastInsertId(), $data);
+        } catch (\Throwable $th) {
+            $this->pdo->rollBack();
+
+            throw $th;
+        }
+    }
+
+    private function buildDemanda(int $id, array $data): Demanda
+    {
+
+        $demanda = new Demanda(
+            $data['titulo'],
+            $data['cep'],
+        );
+
+        $demanda->setId($id);
+
+        return $demanda;
+    }
+}
---
+++ backend/repositories/FornecedorRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace backend\repositories;
+
+require_once dirname(dirname(__FILE__)) . '\db\config\connection.php';
+require_once dirname(dirname(__FILE__)) . '\models\Fornecedor.php';
+
+use backend\db\config\Connection;
+use backend\models\Fornecedor;
+use PDO;
+
+class FornecedorRepository
+{
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Connection::Connect();
+    }
+
+    public function create(array $data)
+    {
+        try {
+
+            $this->pdo->beginTransaction();
+
+            $createStatement = $this->pdo->prepare('INSERT INTO fornecedores (razao_social, cep) VALUES (:razao_social, :cep)');
+            $createStatement->bindParam(':razao_social', $data['razao_social']);
+            $createStatement->bindParam(':cep', $data['cep']);
+
+            $createStatement->execute();
+
+            $this->pdo->commit();
+
+            return $this->buildFornecedor($this->pdo->lastInsertId(), $data);
+        } catch (\Throwable $th) {
+            $this->pdo->rollBack();
+
+            throw $th;
+        }
+    }
+
+    private function buildFornecedor(int $id, array $data): Fornecedor
+    {
+
+        $Fornecedor = new Fornecedor(
+            $data['razao_social'],
+            $data['cep'],
+        );
+
+        $Fornecedor->setId($id);
+
+        return $Fornecedor;
+    }
+}
