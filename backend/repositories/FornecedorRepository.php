39676f4126faa696ed7d0efcff395e4d94c36ae5
Pedro-Benevides <pvsbenevides197@gmail.com>
2025-04-29 00:50:07 -0300

    feat(FornecedorRepository): implementa consulta por demanda

--- backend/repositories/FornecedorRepository.php
+++ backend/repositories/FornecedorRepository.php
@@ -40,6 +40,21 @@ class FornecedorRepository
         }
     }
 
+    public function getByDemanda(int $demandaId)
+    {
+        $selectStatement = $this->pdo->prepare('
+            SELECT fornecedores.id, fornecedores.razao_social, fornecedores.cep 
+            FROM fornecedores
+            INNER JOIN demandas_fornecedores ON fornecedores.id = demandas_fornecedores.fornecedor_id
+            WHERE demandas_fornecedores.demanda_id = :demandaId
+        ');
+
+        $selectStatement->bindParam(':demandaId', $demandaId);
+        $selectStatement->execute();
+
+        return $selectStatement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     private function buildFornecedor(int $id, array $data): Fornecedor
     {
 
